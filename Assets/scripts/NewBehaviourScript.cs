da5cb31303dddac357294459a62b8428d21254f4
Álvaro <alvarocc948@gmail.com>
2022-10-06 09:52:51 +0200

    Initial commit

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Prefabs;
+    public int prefabs;
+
+    void Start()
+    {
+        for(int i = 0; i < prefabs; i++)
+        {
+            var position = new Vector3(Random.Range(-10f, 10f), Random.Range(-5, 5), Random.Range(-10f, 10));
+            Instantiate(Prefabs, position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
