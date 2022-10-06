3d9856da19381c9820ed0d44eeb05a134b115a2b
Álvaro <alvarocc948@gmail.com>
2022-10-06 10:46:37 +0200

    Cambios 1.1

--- Assets/scripts/NewBehaviourScript.cs
+++ Assets/scripts/NewBehaviourScript.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject Prefabs;
-    public int prefabs;
 
     void Start()
     {
-        for(int i = 0; i < prefabs; i++)
+        for(int i = 0; i<10; i++)
         {
-            var position = new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), Random.Range(-10, 10));
+            var position = new Vector3(Random.Range(-20, 20), Random.Range(-5, 5), Random.Range(-10, 10));
             Instantiate(Prefabs, position, Quaternion.identity);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
