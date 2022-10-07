659d83a7d91fa8599eda97e9657ba4f1ff21b115
Álvaro <alvarocc948@gmail.com>
2022-10-07 11:51:06 +0200

    Rename script

--- Assets/scripts/script_game.cs
+++ Assets/scripts/script_game.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class script_game : MonoBehaviour
 {
     public GameObject Prefabs;
 
