83662b1dfdeb67be088bb93a3809d7a3bd49637f
Álvaro <alvarocc948@gmail.com>
2022-10-07 11:55:48 +0200

    Comentarios
    Añado un comentario en aquello que no he visto en otras ocasiones, me sirve como recordatorio.

--- Assets/scripts/script_game.cs
+++ Assets/scripts/script_game.cs
@@ -7,9 +7,10 @@ public class script_game : MonoBehaviour
     public GameObject Prefabs;
 
     void Start()
-    {
+    {   
         for(int i = 0; i<10; i++)
-        {
+        {   
+            //Random.Range lo que hace es que el prefabs aparezca dentro del rango que hemos dado en los ejes XYZ.
             var position = new Vector3(Random.Range(-30, 30), Random.Range(5, 10), Random.Range(-30, 30));
             Instantiate(Prefabs, position, Quaternion.identity);
         }
